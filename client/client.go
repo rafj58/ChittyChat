0923a865284685e93fe44c4f1d8fcfd464dcdf41
teomormi <matteo.mormile@mail.polimi.it>
2023-10-20 18:59:02 +0200

    Create pub-sub bidirectional architecture

--- client/client.go
+++ client/client.go
@@ -1,1 +1,162 @@
 package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+
+	proto "ChittyChat/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	name       string
+	address    string
+	portNumber int
+}
+
+const ( // connection status
+	Connect    int = 0
+	Disconnect     = 1
+	Publish        = 2
+)
+
+var (
+	clientPort = flag.Int("cPort", 5500, "client port")
+	serverPort = flag.Int("sPort", 1000, "server port")
+)
+
+func main() {
+
+	// Parse the flags to get the port for the client
+	flag.Parse()
+
+	// sets the logger to use a log.txt file instead of the console
+	//f := setLog()
+	//defer f.Close()
+
+	// Create a client
+	client := &Client{
+		name:       "client",
+		address:    "127.0.0.1",
+		portNumber: *clientPort,
+	}
+
+	// Connect and publish message
+	connectAndPublish(client)
+}
+
+func connectAndPublish(client *Client) {
+
+	// Connect to the server
+	serverConnection := connectToServer()
+
+	// Get a stream to the server
+	stream, err := serverConnection.SendMessage(context.Background())
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Connect message from client to server
+	connectMessage := &proto.Message{
+		Text: "Connect message from client",
+		Type: int32(Connect),
+		ClientReference: &proto.ClientReference{
+			ClientAddress: client.address, // modify in case of global test
+			ClientPort:    int32(client.portNumber),
+		},
+	}
+
+	if err := stream.Send(connectMessage); err != nil {
+		log.Fatalf("Error while sending connection message: %v", err)
+	}
+
+	// wait for go routine
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Create go-routine for reading messages from server
+	go func() {
+		me := client.address + ":" + strconv.Itoa(client.portNumber)
+
+		for {
+			msg, err := stream.Recv()
+			if err != nil {
+				log.Fatalf("Error while receiving message: %v", err)
+			}
+			clientReference := msg.ClientReference.ClientAddress + ":" + strconv.Itoa(int(msg.ClientReference.ClientPort))
+			if me == clientReference { // i receive the disconnected message that i sent
+				defer wg.Done() // inform the main thread to terminate
+				break
+			} else {
+				log.Printf("[%s] sent %s\n", clientReference, msg.Text)
+			}
+
+		}
+	}()
+
+	for {
+		var text string
+		log.Printf("Enter the content of the message ('exit' to quit): ")
+		fmt.Scanln(&text)
+		if text == "exit" {
+			break
+		}
+
+		// Send message to server
+		err := stream.Send(&proto.Message{
+			Text: text,
+			Type: int32(Publish),
+			ClientReference: &proto.ClientReference{
+				ClientAddress: "127.0.0.1", // modify in case of global test
+				ClientPort:    int32(client.portNumber),
+			},
+		})
+		if err != nil {
+			log.Fatalf("Errore durante l'invio di un messaggio: %v", err)
+		}
+	}
+
+	// Disconnect message from client
+	diconnectMessage := &proto.Message{
+		Text: "disconnect message",
+		Type: int32(Disconnect),
+		ClientReference: &proto.ClientReference{
+			ClientAddress: "127.0.0.1", // modify in case of global test
+			ClientPort:    int32(client.portNumber),
+		},
+	}
+	if err := stream.Send(diconnectMessage); err != nil {
+		log.Fatalf("Error while sending disconnection message: %v", err)
+	}
+
+	// wait for response to disconnect message
+	wg.Wait()
+}
+
+func connectToServer() proto.ChittyChatServiceClient {
+	// Dial the server at the specified port.
+	conn, err := grpc.Dial("127.0.0.1:"+strconv.Itoa(*serverPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Could not connect to port %d", *serverPort)
+	} else {
+		log.Printf("Connected to the server at port %d\n", *serverPort)
+	}
+	return proto.NewChittyChatServiceClient(conn)
+}
+
+func setLog() *os.File {
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	return f
+}
--- server/server.go
+++ server/server.go
@@ -1,52 +1,153 @@
 package main
 
 import (
-	proto "ChittyChat/grpc"
 	"flag"
+	"fmt"
 	"log"
 	"net"
+	"os"
 	"strconv"
+	"sync"
+
+	proto "ChittyChat/grpc"
 
 	"google.golang.org/grpc"
 )
 
+const ( // connection status
+	Connect    int = 0
+	Disconnect     = 1
+	Publish        = 2
+)
+
 type Server struct {
-	proto.UnimplementedProtoServer
-	name string
-	port int
+	proto.UnimplementedChittyChatServiceServer
+	name             string
+	port             int
+	clientReferences map[string]proto.ChittyChatService_SendMessageServer
+	mutex            sync.Mutex // avoid race condition
 }
 
-var port = flag.Int("port", 0, "server port number")
+// flags are used to get arguments from the terminal. Flags take a value, a default value and a description of the flag.
+var port = flag.Int("port", 1000, "server port") // set with "-port <port>" in terminal
 
 func main() {
+
+	// Get the port from the command line when the server is run
 	flag.Parse()
 
-	server := &Server{
-		name: "ChittyChat",
-		port: *port,
-	}
+	// Start the server
+	go startServer()
 
-	go startServer(server)
-
+	// Keep the server running until it is manually quit
 	for {
 
 	}
 }
 
-func startServer(server *Server) {
+func startServer() {
+	// Create a server struct
+	server := &Server{
+		name: "server",
+		port: *port,
+	}
+
+	// Create a new grpc server
 	grpcServer := grpc.NewServer()
 
-	listener, err := net.Listen("tcp", ":"+strconv.Itoa(server.port))
+	// Make the server listen at the given port (convert int port to string)
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", strconv.Itoa(server.port)))
 
 	if err != nil {
 		log.Fatalf("Could not create the server %v", err)
 	}
-	log.Printf("Started %s server at port: %d\n", server.name, server.port)
+	log.Printf("Started server at port: %d\n", server.port)
 
-	proto.RegisterProtoServer(grpcServer, server)
+	// Register the grpc service
+	proto.RegisterChittyChatServiceServer(grpcServer, server)
 	serveError := grpcServer.Serve(listener)
 
 	if serveError != nil {
 		log.Fatalf("Could not serve listener")
 	}
+}
+
+func (s *Server) SendMessage(stream proto.ChittyChatService_SendMessageServer) error {
+
+	if s.clientReferences == nil {
+		// initialize map
+		s.clientReferences = make(map[string]proto.ChittyChatService_SendMessageServer)
+	}
+
+	run := true
+
+	for run {
+		msg, err := stream.Recv()
+
+		// client reference as a String
+		clientString := msg.ClientReference.ClientAddress + ":" + strconv.Itoa(int(msg.ClientReference.ClientPort))
+		log.Printf("Received message from %s", clientString)
+
+		if err != nil {
+			log.Printf("Error while receiving message %v", err)
+			break
+		}
+
+		switch int(msg.Type) {
+		case Connect:
+			{
+				// add to map
+				s.clientReferences[clientString] = stream
+				break
+			}
+
+		case Disconnect:
+			{
+				// forward the disconnect
+				for clientRef, clientStream := range s.clientReferences {
+					err = clientStream.Send(msg)
+					if err != nil {
+						log.Printf("Error during forwarding disconnection message to %s: %v", clientRef, err)
+						break
+					}
+				}
+				// remove stream from map
+				delete(s.clientReferences, clientString)
+				log.Printf("Client %s disconnected", clientString)
+				run = false
+				break
+			}
+
+		case Publish:
+			{
+				for clientRef, clientStream := range s.clientReferences {
+					if clientRef != clientString { // Do not forward the message to the original sender
+						err = clientStream.Send(msg)
+						if err != nil {
+							log.Printf("Error during forwarding to %s: %v", clientRef, err)
+							break
+						}
+					}
+				}
+				break
+			}
+		}
+	}
+	return nil
+}
+
+// sets the logger to use a log.txt file instead of the console
+func setLog() *os.File {
+	// Clears the log.txt file when a new server is started
+	if err := os.Truncate("log.txt", 0); err != nil {
+		log.Printf("Failed to truncate: %v", err)
+	}
+
+	// This connects to the log file/changes the output of the log informaiton to the log.txt file.
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	return f
 }
